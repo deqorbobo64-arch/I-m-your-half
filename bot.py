4acb8d804b7360a1774831111f6ef118c0073f1b
deqorbobo64-arch <deqorbobo64@gmail.com>
2026-04-14 11:24:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -181,5 +181,4 @@ def run_bot():
     print("✅ Bot ishlamoqda...")
     bot.infinity_polling()
 
-threading.Thread(target=run_bot, daemon=True).start()
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+run_bot()
